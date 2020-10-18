92a329ce1f83f43db8667728a816a78b450bbe04
Kletsov, Sergey <Sergey.Kletsov>
2020-10-18 17:05:06 +0300

    first commit

---
+++ var.tf
@@ -0,0 +1,10 @@
+
+variable "clients_whitelist_ip" {
+    description = "whitelist"
+    default = ["8.8.8.8/24", "8.8.8.8/24"]
+}
+
+variable "network_id" {
+    description = "network_id"
+    default = "enprje4rmqdrfcg9hu42"
+}
